7c2d855ba62a92a99658da561b27b351e4083bcb
francescobillia <107175424+Orestinaa@users.noreply.github.com>
2023-05-11 23:16:44 +0200

    Creato file per la connessione al database

---
+++ dbConnection.php
@@ -0,0 +1,16 @@
+<?php
+    $username = "c21patrick";
+    $password = "Udinese2020";
+    $host = "localhost";
+    $dbname = "c21MoneyManager";
+
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        echo "Connection failed";
+    }
+    else {
+        echo "Connected successfully";
+    }
+?>
