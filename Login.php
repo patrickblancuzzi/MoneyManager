f0c7867664ba0efc4bcfb39c9862b32a58db32fd
francescobillia <107175424+Orestinaa@users.noreply.github.com>
2023-05-14 11:56:19 +0200

    Aggiunto reCaptcha v3

--- Login.php
+++ Login.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./imgs/svg/logo-no-background.svg">
     <link rel="stylesheet" href="./styles/styleLogin.css">
-    <title>Login - Money Manager</title>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <title>Login</title>
 </head>
 <body data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="login">
         <section class="sectionSingUp">  
             <div class="formContainer">
-                <form action="Convalidation/convalidateLogin.php" method="post" class="loginForm"> 
+            <?php
+                require_once __DIR__ . '/Convalidation/Config.php';
+            ?>
+                <form action="Convalidation/convalidateLogin.php" method="post" class="loginForm" id="frm"> 
                     <h2>Login</h2> 
                     <label for="username">Username:</label> 
                     <input type="text" id="username" name="username" required> 
@@ -29,11 +33,23 @@
                                         document.getElementById('password').style.border = '1px solid red';
                                     </script>";
                             }
+                            else if($_GET["error"] == 2){
+                                echo "<p class='error'>Captcha non superato</p>";
+                            }
                         }
                     ?>
 
-                    <input type="submit" value="Accedi"> 
+                    <div class="row">
+                    <input type="submit" class="g-recaptcha full-width"
+                        data-sitekey="<?php echo Config::GOOGLE_RECAPTCHA_SITE_KEY; ?>"
+                        data-callback='onSubmit' data-action='submit'
+                        value="Accedi" 
+                        id="btninp"
+                        style="">
+                    </div>
+                    <!-- <input type="submit" value="Accedi"> -->
                     <a href="./SingUp.php" class="linkReg"> Non hai un account? Registrati!</a>
+
                 </form> 
             </div>
             <div class="decorationDiv"></div>
@@ -43,5 +59,20 @@
         </section> 
     </main>
     <script type="module" src="./scripts/main.js"></script>
+
+    <script>
+// JavaScript
+        function onSubmit(token) {
+            var button = document.createElement('input');
+            button.type = 'hidden';
+            button.name = 'recaptcha_token';
+            button.value = token;
+
+            var form = document.getElementById("frm");
+            form.appendChild(button);
+            form.submit();
+        }
+    </script>
+
 </body>
 </html>
