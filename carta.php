d2fba0dc47146bd1bbe297b06b910e6e5740be66
Patrick Blancuzzi <77357177+patrickblancuzzi@users.noreply.github.com>
2023-05-17 14:49:50 +0200

    Update Carta

--- carta.php
+++ carta.php
@@ -36,7 +36,7 @@
                     </div>
                 </li>
                 <li class="itemList">
-                    <a href="#">
+                    <a href="./wishlist.php">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
                         </svg>   
@@ -103,6 +103,24 @@
             </div>
         </div>
     </section>
+
+    <section class="relativeSection">
+        <div class="contentForm">
+            <form class="formWish" action="aggiungiProdotto.php" method="post">
+                <h2>Aggiungi prodotto:</h2>
+                <div class="content">
+                    <label for="prezzo"></label>
+                    <input type="number" step="any" name="entrata" id="entrata" placeholder="Entrata" required>
+                    <label for="prezzo"></label>
+                    <input type="number" step="any" name="uscita" id="uscita" placeholder="Uscita" required>
+                    <label for="data"></label>
+                    <input type="date" name="data" id="data" placeholder="Data" required>
+                    <button class="buttonCrea"type="submit">CREA</button>
+                </div>
+            </form>
+        </div>
+    </section>
+
     <section class="relativeSection">
         <table>
             <thead>
